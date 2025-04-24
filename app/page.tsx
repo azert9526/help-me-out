9be77d90577626355b152277e467df6a41fcd79a
Husăsan Smaranda-Ioana <smara.husasan@gmail.com>
2025-04-24 20:56:31 +0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,92 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import React, { useState } from 'react';
+import { Box, Typography, Avatar, Stack, Divider, Paper, Button} from '@mui/material';
+import dayjs, { Dayjs } from 'dayjs';
+import { useRouter } from 'next/navigation';
+
+export default function ProfilePage() {
+  const router = useRouter();
+
+  const [user] = useState<{
+    firstname: string;
+    lastname: string;
+    email: string;
+    bio: string;
+    avatar: string;
+    birthdate: Dayjs | null;
+    skills: string[];
+  }>({
+    firstname: 'prenume',
+    lastname: 'nume',
+    email: 'email',
+    bio: 'bio',
+    avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQADjfoADAlJPrsl_hiiOMeE-FBor-i6hEAVg&s',
+    birthdate: dayjs('1992-05-10'),
+    skills: ['medicine', 'programming', 'machine learning', 'public speaking'],
+  });
+
+  const handleEditClick = () => {
+    router.push('/profile');
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <Box sx={{ p: 4, display: 'flex', justifyContent: 'center' }}>
+      <Paper elevation={4} sx={{ p: 4, borderRadius: 4, maxWidth: 1000, width: '100%', display: 'flex', gap: 4}}>
+        <Button
+          variant="contained"
+          sx={{ position: 'absolute', top: 16, right: 16 }}
+          onClick={handleEditClick}
+        >
+          Edit profile
+        </Button>
+        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh'}}>
+          <Avatar
+            src={user.avatar}
+            alt={`${user.firstname} ${user.lastname}`}
+            sx={{ width: 200, height: 200 }}
+          />
+        </Box>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh'}}>
+          <Typography variant="h4" fontWeight="bold">
+            {user.firstname} {user.lastname}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" sx={{ mb: 2 }}>
+            {user.email}
+          </Typography>
+
+          <Divider sx={{ my: 2 }} />
+
+          <Typography variant="h6" fontWeight="bold">Bio</Typography>
+          <Typography variant="body1" sx={{ mb: 2 }}>
+            {user.bio}
+          </Typography>
+
+          <Typography variant="h6" fontWeight="bold">Birthday</Typography>
+          <Typography variant="body1" sx={{ mb: 2 }}>
+            {user.birthdate ? user.birthdate.format('DD MMMM YYYY') : 'Nespecificată'}
+          </Typography>
+
+          <Typography variant="h6" fontWeight="bold">Skills</Typography>
+          <Stack direction="row" spacing={1} flexWrap="wrap" sx={{ mt: 1 }}>
+            {user.skills.map((skill, i) => (
+              <Box
+                key={i}
+                sx={{
+                  px: 2,
+                  py: 0.5,
+                  backgroundColor: '#e0e0e0',
+                  borderRadius: '999px',
+                  fontSize: '0.85rem',
+                }}
+              >
+                {skill}
+              </Box>
+            ))}
+          </Stack>
+        </Box>
+      </Paper>
+    </Box>
   );
 }
